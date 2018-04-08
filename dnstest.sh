b874dab568869918c608042597b6c219d25364a5
c1982 <aspsrc@gmail.com>
2018-04-08 21:37:54 +0300

    dns test for Turkish providers

--- dnstest.sh
+++ dnstest.sh
@@ -8,22 +8,31 @@ command -v bc > /dev/null || { echo "bc was not found. Please install bc."; exit
 NAMESERVERS=`cat /etc/resolv.conf | grep ^nameserver | cut -d " " -f 2 | sed 's/\(.*\)/&#&/'`
 
 PROVIDERS="
-1.1.1.1#cloudflare 
-4.2.2.1#level3 
-8.8.8.8#google 
-9.9.9.9#quad9 
-80.80.80.80#freenom 
-208.67.222.123#opendns 
-199.85.126.20#norton 
-185.228.168.168#cleanbrowsing 
-77.88.8.7#yandex 
-176.103.130.132#adguard 
-156.154.70.3#neustar 
-8.26.56.26#comodo
+85.159.66.10#natro 
+185.15.40.80#turdns 
+185.71.216.245#turkishost 
+89.106.14.51#turkticaret 
+93.89.230.241#isimtescil 
+89.163.146.177#kebirhost 
+77.92.152.10#sadecehosting 
+95.130.171.100#karegen
+178.210.170.4#markum
+212.58.4.2#doruknet
+213.142.142.142#websahibi
+77.245.148.3#trdns
+46.20.146.240#kriweb
+178.18.193.35#vargonen
+94.138.196.7#ihs
+31.192.212.5#guzelhost
+91.191.170.177#netdirekt
+95.173.189.2#netinternet
+5.250.241.18#aysima
+5.2.80.11#alastyr
+168.144.228.69#dahanet
 "
 
 # Domains to test. Duplicated domains are ok
-DOMAINS2TEST="www.google.com amazon.com facebook.com www.youtube.com www.reddit.com  wikipedia.org twitter.com gmail.com www.google.com whatsapp.com"
+DOMAINS2TEST="www.google.com amazon.com facebook.com www.youtube.com www.reddit.com wikipedia.org twitter.com gmail.com www.google.com whatsapp.com"
 
 
 totaldomains=0
